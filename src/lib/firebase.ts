6554a2d414104a745f79c9da7d1751b4ac664f20
Cyrus Odg <bossof226@gmail.com>
2025-08-04 19:41:27 +0000

    // Import the functions you need from the SDKs you need import { initial

---
+++ src/lib/firebase.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA1ZRfoKob48xHOVwDHOB4WOa5z5VhRIZQ",
+  authDomain: "allano-ba62a.firebaseapp.com",
+  projectId: "allano-ba62a",
+  storageBucket: "allano-ba62a.firebasestorage.app",
+  messagingSenderId: "913700661186",
+  appId: "1:913700661186:web:1ac3f6021d69ceb6fb985b",
+  measurementId: "G-XBKS2ZKYFK"
+};
+
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+
+export { app, db, auth, storage };
