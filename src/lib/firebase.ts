6b74fcbf8c386de2a70c495abd5c07aff517f073
Cyrus Odg <bossof226@gmail.com>
2025-08-08 18:56:56 +0000

    et pourtant

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -8,7 +9,7 @@ import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: "AIzaSyA1ZRfoKob48xHOVwDHOB4WOa5z5VhRIZQ",
   authDomain: "allano-ba62a.firebaseapp.com",
-  projectId: "allano-ba62a",
+  projectId: "allano",
   storageBucket: "allano-ba62a.firebasestorage.app",
   messagingSenderId: "913700661186",
   appId: "1:913700661186:web:1ac3f6021d69ceb6fb985b",
