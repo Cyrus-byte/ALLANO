a8ddbfafe76a4f91f96d61d6d9971a25c3a0d46d
Cyrus Odg <bossof226@gmail.com>
2025-08-09 08:32:19 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,13 +7,13 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA1ZRfoKob48xHOVwDHOB4WOa5z5VhRIZQ",
-  authDomain: "allano-ba62a.firebaseapp.com",
-  projectId: "allano-ba62a",
-  storageBucket: "allano-ba62a.firebasestorage.app",
-  messagingSenderId: "913700661186",
-  appId: "1:913700661186:web:1ac3f6021d69ceb6fb985b",
-  measurementId: "G-XBKS2ZKYFK"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 
