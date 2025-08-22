a652e8269498dd869ef9b99612711780bd344a62
Cyrus Odg <bossof226@gmail.com>
2025-08-22 13:01:22 +0000

    [12:57:55.932] Running build in Washington, D.C., USA (East) – iad1 [12:

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,4 +1,5 @@
 import {genkitAPIHandler} from '@genkit-ai/next';
-import '@/ai/flows/*.ts';
+import '@/ai/flows/product-recommendation';
+import '@/ai/flows/visual-search-flow';
 
 export const POST = genkitAPIHandler();
