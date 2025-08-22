929b03c1e36002e6976b5486f927ec14a8d3d68e
Cyrus Odg <bossof226@gmail.com>
2025-08-22 11:33:29 +0000

    ?

--- api/cinetpay-notify/route.ts
+++ api/cinetpay-notify/route.ts
@@ -15,6 +15,8 @@ if (!getApps().length) {
 }
 const db = getFirestore();
 
+export const dynamic = 'force-dynamic';
+
 export async function POST(req: Request) {
   try {
     const body = await req.json();
